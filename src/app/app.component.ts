02d1df39a6a3199bbb188af038207f5b1b9e0235
Lucas Juvito <lucas.juvito@cocobambu.com>
2022-06-04 15:43:46 -0300

    Create photo component, photos module and get photos of web api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+interface PhotoArray {
+  allowComments: boolean;
+  comments: number;
+  description: string;
+  id: number;
+  likes: number;
+  postDate: string;
+  url: string;
+  userId: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = 'alurapic';
+  // photos = [
+  //   {
+  //     url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
+  //     description: 'Leão',
+  //   },
+  //   {
+  //     url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg',
+  //     description: 'Leoa',
+  //   },
+  // ];
+
+  photos: PhotoArray[] = [];
+
+  constructor(http: HttpClient) {
+    http.get<PhotoArray[]>('http://localhost:3000/flavio/photos').subscribe(
+      (photos) => {
+        console.log(photos);
+        this.photos = photos;
+      }
+      // (err) => {
+      //   console.error(err.message);
+      // }
+    );
+  }
 }
