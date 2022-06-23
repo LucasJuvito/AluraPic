55cea4904225436cd75693cd35679f91eef0dde5
Lucas Juvito <lucasjuvito@gmail.com>
2022-06-23 15:36:38 -0300

    fix routing

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
-  selector: 'app-photo-list',
+  selector: 'ap-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.sass'],
 })
 export class PhotoListComponent implements OnInit {
   photos: Photo[] = [];
 
-  constructor(private photoService: PhotoService) {}
+  constructor(
+    private photoService: PhotoService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    const userName = this.activatedRoute.snapshot.params['userName'];
+
+    console.log(userName);
+
     this.photoService
-      .listFromUser('flavio')
+      .listFromUser(userName)
       .subscribe((photos) => (this.photos = photos));
   }
 }
