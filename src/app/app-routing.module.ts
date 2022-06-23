9f9c75a913464a3731a79f42d9dc0932ba14b47c
Lucas Juvito <lucasjuvito@gmail.com>
2022-06-23 14:46:57 -0300

    componentizing and routing the application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'user/flavio', component: PhotoListComponent },
+  { path: 'p/add', component: PhotoFormComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
